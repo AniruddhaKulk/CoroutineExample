72fb14ef4412fa7d868ad570da73db939b593a4f
AniruddhaKulk <anikulki@gmail.com>
2020-07-30 09:08:53 +0530

    Add ViewModelFactory class

---
+++ app/src/main/java/com/anikulki/coroutineexample/ui/base/ViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.anikulki.coroutineexample.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.anikulki.coroutineexample.data.api.ApiHelper
+import com.anikulki.coroutineexample.data.repository.NewsListRepositoryImpl
+import com.anikulki.coroutineexample.ui.newslist.viewmodel.MainViewModel
+import java.lang.IllegalStateException
+
+class ViewModelFactory(private val apiHelper: ApiHelper): ViewModelProvider.Factory {
+
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(MainViewModel::class.java)){
+            return MainViewModel(NewsListRepositoryImpl(apiHelper)) as T
+        }
+
+        throw IllegalStateException("Unknown class name")
+    }
+
+
+}
